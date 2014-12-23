d43c7da288acc762b5cd4e7a2a90fe5f9d7e678f
oragerin <oragerin@gmail.com>
2014-12-23 20:33:14 +0200

    Update free.pas

--- free.pas
+++ free.pas
@@ -4,13 +4,13 @@ const d: array[1..8,1..2] of Integer =
 var i: Integer;
     dx, dy: Integer;
 begin
-  if (x > 0) and (x < 11) and (y > 0) and (y < 11) and (Pole[x,y] = 0) then
+  if (x > 0) and (x < 11) and (y > 0) and (y < 11) and (Field[x,y] = 0) then
   begin
     for i := 1 to 8 do
     begin
       dx := x + d[i,1];
       dy := y + d[i,2];
-      if (dx > 0) and (dx < 11) and (dy > 0) and (dy < 11) and (Pole[dx,dy] > 0) then
+      if (dx > 0) and (dx < 11) and (dy > 0) and (dy < 11) and (Field[dx,dy] > 0) then
       begin
         Result := False;
         Exit;
